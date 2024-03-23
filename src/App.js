419162809b15a776c260c4f8c72ba184f7f03b72
debnathsubhankar <subhankardebnat923262@gmail.com>
2024-03-23 21:54:42 +0530

    add log in page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Component/Header";
 import "bootstrap/dist/css/bootstrap.css";
+import Loging from "./Component/Loging";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Loging />
     </div>
   );
 }
--- src/Component/Loging.js
+++ src/Component/Loging.js
@@ -1,6 +1,26 @@
 import React from "react";
 
 const Loging = () => {
-  return <div></div>;
+  return (
+    <div className="container mt-5">
+      <div className="card m-auto col-sm-6 p-3">
+        <div className="m-auto">
+          <span className="mt-2">
+            <label htmlFor="email">Enter Email :</label>
+            <input type="text" name="Email" id="email" />
+          </span>
+          <br />
+          <span className="mt-2">
+            <label htmlFor="password">Enter Password :</label>
+            <input type="password" name="password" id="password" />
+          </span>
+          <br />
+          <span className="mt-2">
+            <button className="btn btn-primary">Login</button>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Loging;
