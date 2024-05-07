2d7f9db924be493a52d0713553fbea25a192e1ff
debnathsubhankar <subhankardebnat923262@gmail.com>
2024-05-08 04:29:01 +0530

    save with a error of firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./Component/Header";
 import "bootstrap/dist/css/bootstrap.css";
-import Loging from "./Component/Loging";
+// import Loging from "./Component/Loging";
 import Signup from "./Component/Signup";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
@@ -11,8 +11,8 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" exact Component={Loging} />
-          <Route path="/signup" Component={Signup} />
+          {/* <Route path="/loging" Component={Loging} /> */}
+          <Route path="/" exact Component={Signup} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Store/Slices/authSlice.js
+++ src/Store/Slices/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import firebase from "../Index";
+import { firebase } from "firebase/app";
 import "firebase/auth";
 
 // for firebase login
