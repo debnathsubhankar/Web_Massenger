652ff07d8f69a82c836826716702d74cdc21ace2
debnathsubhankar <subhankardebnat923262@gmail.com>
2024-06-11 01:40:29 +0530

    add new code

--- src/Component/Chat/ChatBox.js
+++ src/Component/Chat/ChatBox.js
@@ -6,10 +6,10 @@ const ChatBox = () => {
           <img src="./user.png" alt="user" />
         </div>
         <div className="user_spec col-sm-6">
-          <p>Name</p>
-          <span>Lorem ipsum dolor.</span>
+          <p className="mb-0">Name</p>
+          <p>Lorem ipsum dolor.</p>
         </div>
-        <div className="chat_icons col-sm-4">
+        <div className="chat_icons col-sm-3">
           <span>
             <img src="./call.png" alt="call" />
           </span>
@@ -19,6 +19,27 @@ const ChatBox = () => {
           <span>
             <img src="./about.png" alt="about" />
           </span>
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="lower_div">
+        <div className="icons col-sm-3">
+          <span>
+            <img src="./gallery.png" alt="gallery" />
+          </span>
+          <span>
+            <img src="./camera.png" alt="camera" />
+          </span>
+          <span>
+            <img src="./microphone.png" alt="mic" />
+          </span>
+        </div>
+        <div className="text_area col-sm-9">
+          <input type="text" />
+          <span>
+            <img src="./emoji.png" alt="emoji" />
+          </span>
+          <button className="btn btn-primary">Send</button>
         </div>
       </div>
     </div>
