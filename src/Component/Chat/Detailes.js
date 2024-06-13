5221f3134dd36aba1a5b0732e0df51c7895a4c04
debnathsubhankar <subhankardebnat923262@gmail.com>
2024-06-14 02:21:43 +0530

    craet details page

--- src/Component/Chat/Detailes.js
+++ src/Component/Chat/Detailes.js
@@ -1,5 +1,74 @@
 const Detailes = () => {
-  return <div>Detailes</div>;
+  return (
+    <div>
+      <div className="details_user d-flex mt-5">
+        <span>
+          <img src="./user.png" alt="" />
+        </span>
+        <span>
+          <h3>Name Surname</h3>
+        </span>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="options d-flex">
+        <div className="opt d-flex">
+          <span>
+            <p>Chat Settings</p>
+          </span>
+          <span>
+            <img src="./down.png" alt="down" />
+          </span>
+        </div>
+        <div className="opt d-flex">
+          <span>
+            <p>Privecy & help</p>
+          </span>
+          <span>
+            <img src="./down.png" alt="down" />
+          </span>
+        </div>
+
+        <div className="opt d-flex">
+          <span>
+            <p>Shear phots</p>
+          </span>
+          <span>
+            <img src="./down.png" alt="down" />
+          </span>
+        </div>
+        <div className="images d-flex mb-5">
+          <div className="d-flex sub-img">
+            <span>
+              <img src="nature.jpg" alt="" />
+            </span>
+            <p>ima.2155487.jpg</p>
+            <div className="d_icons">
+              <img src="./download.png" alt="download" />
+            </div>
+          </div>
+          <div className="d-flex sub-img">
+            <span>
+              <img src="nature.jpg" alt="" />
+            </span>
+            <p>ima.2155487.jpg</p>
+            <div className="d_icons">
+              <img src="./download.png" alt="download" />
+            </div>
+          </div>
+          <div className="d-flex sub-img">
+            <span>
+              <img src="nature.jpg" alt="" />
+            </span>
+            <p>ima.2155487.jpg</p>
+            <div className="d_icons">
+              <img src="./download.png" alt="download" />
+            </div>
+          </div>
+        </div>
+        <button className="btn btn-danger">Block Contact</button>
+      </div>
+    </div>
+  );
 };
 
 export default Detailes;
